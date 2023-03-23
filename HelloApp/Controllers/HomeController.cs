7abdf7531c779abd6ea8c504c9238c2d46db49db
nkorotkyi <zipz211_kmo@student.ztu.edu.ua>
2023-03-23 18:49:28 +0200

    First app done

---
+++ HelloApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string name)
+        {
+            string authData = $"Hello, {name}";
+            return Content(authData);
+        }
+    }
+}
